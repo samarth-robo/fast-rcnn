079805e98ecfec2f2533c0179b449940639886d8
Samarth Brahmbhatt <samarth.robo@gmail.com>
2016-02-17 11:31:48 -0500

    Trained with pooling and projection steps exchanged for seg, drops performance by 0.1%

--- experiments/scripts/context_vgg16.sh
+++ experiments/scripts/context_vgg16.sh
@@ -9,13 +9,12 @@ LOG="experiments/logs/context_vgg16.txt.`date +'%Y-%m-%d_%H-%M-%S'`"
 exec &> >(tee -a "$LOG")
 echo Logging output to "$LOG"
 
-time ./tools/train_net.py --gpu 0 \
-  --solver models/VGG16/solver.prototxt \
-  --weights ~/research/dextro/dextro-tools/deep_context/models/det_seg_frcnn.caffemodel \
-  --imdb voc_2007_trainval
+# time ./tools/train_net.py --gpu 0 \
+#   --solver models/VGG16/solver.prototxt \
+#   --weights ~/research/dextro/dextro-tools/deep_context/models/det_seg_frcnn.caffemodel \
+#   --imdb voc_2007_trainval
 
 time ./tools/test_net.py --gpu 0 \
   --def models/VGG16/test_context.prototxt \
   --net output/default/voc_2007_trainval/snapshots/vgg16/_iter_40000.caffemodel \
-  --imdb voc_2007_test
-  --comp
+  --imdb voc_2007_test --comp
