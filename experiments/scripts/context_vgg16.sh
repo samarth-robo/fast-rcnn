bf3e0852d28c19d79c77015b1365a460a6530d90
Samarth Brahmbhatt <samarth.robo@gmail.com>
2016-02-19 12:16:07 -0500

    Training with expansion of 20% and seg w/o holes gives 67.1%

--- experiments/scripts/context_vgg16.sh
+++ experiments/scripts/context_vgg16.sh
@@ -5,14 +5,15 @@ set -e
 
 export PYTHONUNBUFFERED="True"
 
-LOG="experiments/logs/context_vgg16.txt.`date +'%Y-%m-%d_%H-%M-%S'`"
+# LOG="experiments/logs/context_vgg16.txt.`date +'%Y-%m-%d_%H-%M-%S'`"
+LOG="experiments/logs/context_vgg16.txt"
 exec &> >(tee -a "$LOG")
 echo Logging output to "$LOG"
 
-# time ./tools/train_net.py --gpu 0 \
-#   --solver models/VGG16/solver.prototxt \
-#   --weights ~/research/dextro/dextro-tools/deep_context/models/det_seg_frcnn.caffemodel \
-#   --imdb voc_2007_trainval
+time ./tools/train_net.py --gpu 0 \
+  --solver models/VGG16/solver.prototxt \
+  --weights ~/research/dextro/dextro-tools/deep_context/models/det_seg_frcnn.caffemodel \
+  --imdb voc_2007_trainval
 
 time ./tools/test_net.py --gpu 0 \
   --def models/VGG16/test_context.prototxt \
