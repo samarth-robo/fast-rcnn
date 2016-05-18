8f512548fe2291fd4ee53fd5da13eeb312d3e70f
Samarth Brahmbhatt <samarth.robo@gmail.com>
2016-05-18 14:57:16 -0400

    Running context experiments on pascal voc 2007

--- experiments/scripts/context_vgg16.sh
+++ experiments/scripts/context_vgg16.sh
@@ -6,16 +6,16 @@ set -e
 export PYTHONUNBUFFERED="True"
 
 # LOG="experiments/logs/context_vgg16.txt.`date +'%Y-%m-%d_%H-%M-%S'`"
-LOG="experiments/logs/context_0.25_vgg16.txt"
+LOG="experiments/logs/context_1.0_vgg16.txt"
 exec &> >(tee -a "$LOG")
 echo Logging output to "$LOG"
 
 time ./tools/train_net.py --gpu 0 \
-  --solver models/VGG16/solver.prototxt \
+  --solver models/VGG16/solver_context.prototxt \
   --weights data/fast_rcnn_models/frcnn_coco_init.caffemodel \
   --imdb voc_2007_trainval
 
 time ./tools/test_net.py --gpu 0 \
   --def models/VGG16/test_context.prototxt \
-  --net output/default/voc_2007_trainval/context_vgg16_0.2_iter_40000.caffemodel \
+  --net output/default/voc_2007_trainval/context_vgg16_1.0_iter_40000.caffemodel \
   --imdb voc_2007_test --comp
